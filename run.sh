cd8e1b522b4a65fb85fedd92615ac3addbdd4f7d
bbhsu <brianhsu93@gmail.com>
2018-03-23 00:34:50 -0700

    add run.sh and update truffle.js settings for ganache-cli

---
+++ run.sh
@@ -0,0 +1,13 @@
+# Build and test script
+
+# Compile contracts with truffle
+truffle compile
+
+# Launch local ethereum blockchain in background
+#ganache-cli
+
+# Migrate contracts to blockchain with truffle
+truffle migrate
+
+# Run contract unit tests with truffle
+truffle test
