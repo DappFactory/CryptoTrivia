c19464444fa2acff50995d3e82ce6e01d76be361
bbhsu <brianhsu93@gmail.com>
2018-05-18 08:14:49 -0700

    remove gas and gasLimit from deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,6 @@
 var Quiz = artifacts.require("./Quiz.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Quiz, 5, 1000, {
-    gas: 3000000,
-    gasPrice: web3.toWei("200", "gwei")
-  }).then(
+  deployer.deploy(Quiz, 5, 1000).then(
     () => console.log("Quiz address: " + Quiz.address));
 };
